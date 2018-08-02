66b0bcd0c9951adaed08de82a22556bcdacf950c
oussama CHIBANI <oussama.CHIBANI@yahoo.fr>
2018-08-02 12:07:19 +0200

    First commit version

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main',
+//  templateUrl: './app.component.html',
+template : '<h1> hey {{tittle}}';  
+// styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ODDO BHF';
+}
